7627529147863f590f1cfa6aabc8d5383e010d2b
adrian <agyaniri@gmail.com>
2020-09-28 21:37:43 -0300

    fumadores

--- fumadores.py
+++ fumadores.py
@@ -5,9 +5,11 @@ import time
 papelEnMesa = False
 fosforosEnMesa = False
 tabacoEnMesa = False
+semaforo = threading.Semaphore()
 
 def agente():
     global papelEnMesa, fosforosEnMesa, tabacoEnMesa
+    semaforo.acquire()
     while True:
         caso = random.choice([0,1,2]) #al azar pone dos cosas en la mesa
         if caso == 0:
@@ -24,6 +26,13 @@ def agente():
 def fumadorConPapel():
     while True:
         # si hay fósforos y tabaco en la mesa
+        if 0 :
+            papelEnMesa = False
+            tabacoEnMesa = False
+            print('fumar papel')
+            time.sleep(2)
+            semaforo.release()
+
             # tomarlos
             # armar cigarrillo y fumar: se puede simular con un sleep
             # llamar de nuevo a agente para que reponga en la mesa dos cosas al azar
@@ -32,6 +41,12 @@ def fumadorConPapel():
 def fumadorConFosforos():
     while True:
         # si hay papel y tabaco en la mesa
+        if 1 :
+            papelEnMesa = False
+            tabacoEnMesa = False
+            print('fumar fosforo')
+            time.sleep(2)
+            semaforo.release()
             # tomarlos
             # armar cigarrillo y fumar: se puede simular con un sleep
             # llamar de nuevo a agente para que reponga en la mesa dos cosas al azar
@@ -39,6 +54,12 @@ def fumadorConFosforos():
 def fumadorConTabaco():
     while True:
         # si hay fósforos y papel en la mesa
+        if 2:
+            fosforosEnMesa = False
+            papelEnMesa = False
+            print('fumar tabaco')
+            time.sleep(2)
+            semaforo.release()
             # tomarlos
             # armar cigarrillo y fumar: se puede simular con un sleep
             # llamar de nuevo a agente para que reponga en la mesa dos cosas al azar
